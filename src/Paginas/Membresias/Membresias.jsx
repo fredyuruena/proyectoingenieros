9fafcc8393b911541fc821a36d1aa1a6e44489b9
fredyuruena <fredydurango@ene8.com.mx>
2026-02-27 09:35:42 -0500

    Commit sección membresias JSON/api

--- src/Paginas/Membresias/Membresias.jsx
+++ src/Paginas/Membresias/Membresias.jsx
@@ -1,2 +1,21 @@
-const Membresias = () => <div>Membresias</div>;
+import MembresiasComponent from "../../componentes/Membresias/Membresias";
+import Banner from "../../componentes/Banner/Banner";
+import bannerImage from "../../assets/banner-membresias.jpg"; 
+
+const Membresias = () => {
+  return (
+    <>
+      <Banner
+        title="Impulsando el Comercio y el Turismo en México"
+        subtitle="Tu aliado estratégico para el crecimiento empresarial. Representamos, defendemos y fortalecemos a los sectores productivos del país."
+        backgroundImage={bannerImage}
+        primaryButton={{ text: "Únete a la Cámara", link: "/afiliate" }}
+        secondaryButton={{ text: "Conoce Más", link: "/nosotros" }}
+      />
+
+      <MembresiasComponent />
+    </>
+  );
+};
+
 export default Membresias;
